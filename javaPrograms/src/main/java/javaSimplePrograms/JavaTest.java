171c05b205eca970556dd1928bda961a3eae82f2
Ashish A. Bhalerao <Ashish A. Bhalerao@DELL>
2019-01-05 19:40:34 +0530

    Synechron question on main() metthod.

---
+++ javaPrograms/src/main/java/javaSimplePrograms/JavaTest.java
@@ -0,0 +1,42 @@
+package javaSimplePrograms;
+
+//Synechron Questions===>>>05/01/2019
+
+public class JavaTest {
+	
+	
+/*1. Error: Main method not found in class javaSimplePrograms.JavaTest, please define the main method as:
+    static void main(String[] args)
+or a JavaFX application class must extend javafx.application.Application*/
+//	static void main(String[] args) {
+//		System.out.println("Hi");
+//
+//	}
+	
+	
+/*2. Error: Main method not found in class javaSimplePrograms.JavaTest, please define the main method as:
+    static void main(String[] args)
+or a JavaFX application class must extend javafx.application.Application*/
+//	public static void main() {
+//		System.out.println("Hi");
+//		
+//	}
+	
+	
+/*3. Method must have a return type*/
+//	public static  main(String []args) {
+//		
+//		
+//	}
+	
+
+/*4. Error: Main method not found in class javaSimplePrograms.JavaTest, please define the main method as:
+    static void main(String[] args)
+or a JavaFX application class must extend javafx.application.Application*/	
+//	private static void main(String [] args) {
+//		System.out.println("Hi");
+//		
+//	}
+//	
+	
+}
